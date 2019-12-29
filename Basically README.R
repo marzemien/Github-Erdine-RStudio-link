10fd87d0074d82fb77d67eaf4c089fede51b4971
Michael Rzemien <michael.rzemien@gmail.com>
2019-12-29 13:27:34 -0500

    Next try of commit.

--- Basically README.R
+++ Basically README.R
@@ -1,4 +1,6 @@
 #This is my attempt to link RStudio on Erdine to a repo created on github. 
+#Ok, so the versio control menu doesn't seem be transmitting my changes to it's Github repository. 
+
 
 print("This file was created in RStudio.")
 
